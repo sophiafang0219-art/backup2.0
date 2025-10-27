6d7088d148926871f300d88990a4f4071969b9a0
Sophia <sophiafang0219@gmail.com>
2025-10-27 16:43:31 +0800

    修正

--- all.js
+++ all.js
@@ -1,15 +1,29 @@
 
 // --------產品總表--------------------
-  //篩選
-  function openPopup() {
-    const bk = document.querySelector('#bk');
-    const popup = document.querySelector('#popup');
-    bk.classList.add('active');
-    popup.classList.add('active');
-  }
-  function closePopup() {
-    const bk = document.querySelector('#bk');
-    const popup = document.querySelector('#popup')
-    bk.classList.remove('active');
-    popup.classList.remove('active');
-  }
+//篩選
+function openPopup() {
+  const bk = document.querySelector('#bk');
+  const popup = document.querySelector('#popup');
+  bk.classList.add('active');
+  popup.classList.add('active');
+};
+function closePopup() {
+  const bk = document.querySelector('#bk');
+  const popup = document.querySelector('#popup')
+  bk.classList.remove('active');
+  popup.classList.remove('active');
+};
+// -----------------個別產品頁---------------
+$(function(){
+  // $('img物件').attr(`src`, `路徑+檔名`)
+  $('.pics>img').click(function () {
+    newImg = $(this).attr('src');
+    $('.big').attr('src', newImg);
+  });
+  // $('.pics .up').click(function () {
+  //   let preImg = newImg.prev().attr('src');
+  //   $('.big').attr('src', preImg);
+  // });
+
+
+});
