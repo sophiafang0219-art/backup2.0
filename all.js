4f4bb67a8852ea4dcaeb136f24f1a59dd888cfa0
Sophia <sophiafang0219@gmail.com>
2025-10-29 23:38:48 +0800

    some adjustment

--- all.js
+++ all.js
@@ -1,3 +1,17 @@
+//---------nav hamburger-------------
+$(function () {
+
+  // alert('dasdd')
+
+console.log('start');
+
+})
+// $(".menu").on("click", function () {
+//   $(this).toggleClass('active')
+// });
+// $('.menu').on().click(function () {
+// })
+
 
 // --------產品總表--------------------
 //篩選
@@ -13,43 +27,30 @@ function closePopup() {
   bk.classList.remove('active');
   popup.classList.remove('active');
 };
+//產品列表標題
+
+
+
 
 //產品列表排列
 $(function () {
   function adjustWidth() {
     let windowWidth = $(window).width();
-    console.log('目前視窗寬度:', windowWidth);
-    
-    if (windowWidth <= 800) {
-      console.log('執行 ≤800 的設定');
+    if (windowWidth <= 535) {
+      $('.prodAll h2:nth-child(1)').html(`產品列表<br>（閱讀器及周邊）`)
+      $('.prodAll .list').css('width', '360px');
+    } else if (windowWidth <= 800) {
       $('.prodAll .list').css('width', '360px');
     } else if (windowWidth <= 1170) {
-      console.log('執行 ≤1170 的設定');
       $('.prodAll .list').css('width', 'calc(360px * 2 + 36px)');
     } else {
-      console.log('執行 >1170 的設定');
       $('.prodAll .list').css('width', 'calc(360px * 3 + 36px * 2)');
     }
   }
-  
   adjustWidth();
   $(window).resize(adjustWidth);
 });
-// $(function () {
-//   if ($(window).width() <= 800) {
-//     $('.prodAll list').css('width', '360px')
-//   }
-//   else if ($(window).width() <= 1170) {
-//     $('.prodAll list').css('width', 'calc(360px * 2 + 36px)')
 
-//   }
-// })
-
-// .css({
-// 					left: e.pageX + 10,
-// 					top: e.pageY + 10,
-
-// 				})
 // -----------------個別產品頁---------------
 $(function () {
   // $('img物件').attr(`src`, `路徑+檔名`)
@@ -57,10 +58,4 @@ $(function () {
     newImg = $(this).attr('src');
     $('.big').attr('src', newImg);
   });
-  // $('.pics .up').click(function () {
-  //   let preImg = newImg.prev().attr('src');
-  //   $('.big').attr('src', preImg);
-  // });
-
-
 });
